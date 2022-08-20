63160bbf950620d7542d14f6aa34e3b5c21a7e24
quang1105 <89206951+quang1105@users.noreply.github.com>
2022-08-20 20:56:41 +0700

    20/08/2022

--- Chiatamgiac.java
+++ Chiatamgiac.java
@@ -1,31 +1,25 @@
 
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ADMIN
- */
-public class Chiatamgiac {
+public class ChiaTamgiac {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t --> 0) {
-            int n = sc.nextInt();
-            int h = sc.nextInt();
-            for(int i = 1; i < n; i++) {
-                double a = h * Math.sqrt((double) i / n);
+        Scanner scan=new Scanner(System.in);
+        int t= scan.nextInt();
+        while(t-->0){
+            int n= scan.nextInt();
+            int h= scan.nextInt();
+            for(int i=1;i<n;i++){
+                double a=h*Math.sqrt((double) i/n);
                 DecimalFormat df = new DecimalFormat("#.000000");
-                System.out.println(df.format(a) + " ");
+                
+                System.out.print(df.format(a)+" ");
             }
             System.out.println();
         }
     }
+
+
 }
